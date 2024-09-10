4ff5ce3fbdaedbbf6c96a7813ba70fad4d64c3a7
Jay00 <jasonclarklaw@gmail.com>
2024-09-10 14:41:16 -0400

    upate to add CLI

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
+use clap::builder::styling;
+use clap::builder::Styles;
+use clap::Parser;
+use dunce::canonicalize;
+
 use chrono::NaiveDate;
 use doe::*;
 use regex::Regex;
 use std::{fs, path::PathBuf};
 use walkdir::WalkDir;
+
+fn my_styles() -> Styles {
+    styling::Styles::styled()
+        .header(styling::AnsiColor::Yellow.on_default() | styling::Effects::BOLD)
+        .usage(styling::AnsiColor::Yellow.on_default() | styling::Effects::BOLD)
+        .literal(styling::AnsiColor::Green.on_default() | styling::Effects::BOLD)
+        .placeholder(styling::AnsiColor::Green.on_default())
+}
+
+#[derive(Parser)]
+#[command(styles=my_styles())]
+#[command(arg_required_else_help = true)]
+#[command(author = "Jason K. Clark <jasonclarklaw.com>")]
+#[command(version)]
+#[command(name = "Opinion Renamer")]
+#[command(bin_name = "renameops")]
+#[command(about = "\n\n
+** OPINION RENAMER **
+A utility to rename docx court opinions by date.
+Created by Jason K. Clark", long_about = None)]
+struct Cli {
+    /// Optional path to the directory containing your case file
+    #[arg(short, long, value_name = "FILE", default_value = "./")]
+    path: PathBuf,
+}
 
 fn extract_decision_date(content: &Vec<String>) -> Option<NaiveDate> {
     // Look for "September 20, 2011, Argued; September 6, 2012, Decided"
@@ -38,15 +68,37 @@ fn generate_new_file_name(file_path: &PathBuf, opinion_date: &NaiveDate) -> Path
 }
 
 fn main() {
-    let dir = "C:\\Users\\jason\\Downloads\\third";
+    let cli = Cli::parse();
 
-    println!("Renaming docx files in dir {}", dir);
+    // You can check the value provided by positional arguments, or option arguments
+    let path = cli.path;
+
+    println!("Provided path: {:?}", path);
+
+    let case_file_directory_absolute = canonicalize(path).unwrap();
+
+    // let dir = "C:\\Users\\jason\\Downloads\\third";
+    let dir = case_file_directory_absolute;
+
+    // Date Regex YYYY-MM-DD
+    let re = Regex::new(r"^(\d{4})-(\d{2})-(\d{2})").unwrap();
+
+    println!("Renaming docx files in dir {:?}", dir);
     let walker = WalkDir::new(dir).into_iter();
     for entry in walker.into_iter() {
         if let Ok(e) = entry {
+            let file_name_str = e.file_name().to_string_lossy();
             // We are only looking in docx files
-            if e.file_name().to_string_lossy().ends_with("docx") {
+            if file_name_str.ends_with("docx") {
+                // Check if date string is already appended
+                let x = re.is_match(&file_name_str);
+                if x {
+                    // Skip documents which already have the date
+                    continue;
+                }
+
                 println!("Checking File: {}", e.path().display());
+
                 let file_path = PathBuf::from(e.path());
 
                 // docx::docx_replace("./name.docx", "name", "andrew").unwrap();
